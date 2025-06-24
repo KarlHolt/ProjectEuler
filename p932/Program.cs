f20aa033038898db0f8a02573d8bbb04a2060a2b
KarlHolt <karlimmanuelholt@tutanota.com>
2025-06-24 08:34:00 +0200

    Changed Is2025 Method, from checking all possible splittings of a number to only checking the one that splits the number in equal parts. This is an assumption that is not mentioned in the problem, nor have I made a proof for why this holds, but this change significantly speed up the code.

--- p932/Program.cs
+++ p932/Program.cs
@@ -23,23 +23,20 @@ class Program
     {
         bool is2025 = false;
 
-        for (int i = 1; i < Math.Log10(n); i++)
+        double power = Math.Log10(n);
+        ulong tenth = (ulong)Math.Pow(10, Math.Ceiling(power / 2));
+
+        ulong n1 = n / tenth; // All above 10^i
+        ulong n2 = n % tenth; // All below 10^i
+
+        // If it has been split such that the first integer in n2 is a 0
+        if (n2 < (tenth / 10))
+            return is2025;
+
+        ulong subtotal = n1 + n2;
+        if (subtotal * subtotal == n)
         {
-            ulong tenth = (ulong)Math.Pow(10, i);
-
-            ulong n1 = n / tenth; // All above 10^i
-            ulong n2 = n % tenth; // All below 10^i
-
-            // If it has been split such that the first integer in n2 is a 0
-            if (n2 < (tenth / 10))
-                continue;
-
-            ulong subtotal = n1 + n2;
-            if (subtotal * subtotal == n)
-            {
-                is2025 = true;
-                break;
-            }
+            is2025 = true;
         }
         return is2025;
     }
